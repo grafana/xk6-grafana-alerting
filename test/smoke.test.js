fbebf7175ace67f4df81f592bdf743564bc54bbf
Moustafa Baiou <moustafa.baiou@grafana.com>
2025-12-05 17:00:31 -0500

    feat(test): Add bearer token auth + parse environment variables for smoke test
    This lets us set `GRAFANA_URL`, `GRAFANA_API_TOKEN`, `GRAFANA_ADMIN_USER`, and `GRAFANA_ADMIN_PASSWORD` via environment variables to the test.
    
    Using `GRAFANA_API_TOKEN` will enable bearer token authentication for the smoke test.`

--- test/smoke.test.js
+++ test/smoke.test.js
@@ -4,46 +4,68 @@ import http from "k6/http";
 import encoding from 'k6/encoding';
 import { expect } from "https://jslib.k6.io/k6-testing/0.6.1/index.js";
 
+function envOrDefault(envVarName, def) {
+  const envVar = __ENV[envVarName];
+  return envVar ?? def;
+}
+
+// Ensures simulation has valid parameters and builds a grafana api for
+// simulation testing
+function ensureConfig() {
+  return {
+    url: envOrDefault('GRAFANA_URL', 'http://localhost:3000'),
+    username: envOrDefault('GRAFANA_ADMIN_USER', 'admin'),
+    password: envOrDefault('GRAFANA_ADMIN_PASSWORD', 'admin'),
+    token: envOrDefault('GRAFANA_API_TOKEN', ''),
+  };
+}
+
 export const options = {
   thresholds: {
-    checks: ["rate==1"],
+    'http_req_duration{filter:folder_uid}': ['p(99)<3000'], // 99% of requests must complete below 3s
   },
 }
 
-const GRAFANA_URL = "http://localhost:3000";
-const GRAFANA_ADMIN_USER = "admin";
-const GRAFANA_ADMIN_PASSWORD = "admin";
-
-const commonRequestParams = {
-  auth: 'basic',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Basic ${encoding.b64encode(`${GRAFANA_ADMIN_USER}:${GRAFANA_ADMIN_PASSWORD}`)}`,
+function buildRequestParams(username, password, token) {
+  let params = {
+    auth: 'basic',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Basic ${encoding.b64encode(`${username}:${password}`)}`,
+    }
+  };
+  if (!token) {
+    return params
   }
-};
+  params.headers['Authorization'] = `Bearer ${token}`;
+  delete params.auth
+  return params;
+}
 
 const folderUID = "smoke-test-folder";
 
-function deleteFolder(uid) {
-  let deleteResponse = http.del(`${GRAFANA_URL}/api/folders/${folderUID}?forceDeleteRules=true`, null, commonRequestParams);
+function deleteFolder(url, uid, commonRequestParams) {
+  let deleteResponse = http.del(`${url}/api/folders/${uid}?forceDeleteRules=true`, null, commonRequestParams);
   console.log(`Folder deletion response status: ${deleteResponse.status}`);
   console.log(`Folder deletion response body: ${deleteResponse.body}`);
   return deleteResponse;
 }
 
 export function setup() {
-  // TODO: setup folder
+  const { url, token, username, password } = ensureConfig();
+  let commonRequestParams = buildRequestParams(username, password, token);
+
   let folderReqBody = {
     uid: folderUID,
     title: "Smoke Test Folder",
     description: "Folder created for smoke test of k6 grafana alerting extension",
   }
-  let existingFoldersResp = http.get(`${GRAFANA_URL}/api/folders/${folderUID}`, commonRequestParams);
+  let existingFoldersResp = http.get(`${url}/api/folders/${folderUID}`, commonRequestParams);
   if (existingFoldersResp.status === 200) {
     console.log(`Folder with UID ${folderUID} already exists. Cleaning up before test.`);
-    deleteFolder(folderUID);
+    deleteFolder(url, folderUID, commonRequestParams);
   }
-  let response = http.post(`${GRAFANA_URL}/api/folders`, JSON.stringify(folderReqBody), commonRequestParams)
+  let response = http.post(`${url}/api/folders`, JSON.stringify(folderReqBody), commonRequestParams)
   console.log(`Folder creation response status: ${response.status}`);
   console.log(`Folder creation response body: ${response.body}`);
   // generate a single group with 1 recording and 1 alerting rule in the folder
@@ -56,22 +78,28 @@ export function setup() {
     groupsPerFolder: 1,
     seed: 1764919953738342000,
     uploadConfig: {
-      grafanaURL: GRAFANA_URL,
-      username: GRAFANA_ADMIN_USER,
-      password: GRAFANA_ADMIN_PASSWORD,
+      grafanaURL: url,
+      token: token,
+      username: token ? '' : username,
+      password: token ? '' : password,
       orgId: 1,
       folderUIDs: [folderUID],
     },
   };
   console.log("Generating test data with input:", input);
   let output = GenerateGroups(input);
-  return output;
+  return { output, commonRequestParams, url };
 }
 
-export default function ({ groups, input_config }) {
+export default function ({ output: { groups, inputConfig }, commonRequestParams, url }) {
   // verify the rules are created in grafana prometheus api as expected
-  console.log("Verifying created rules in Grafana", input_config);
-  let prometheusResponse = http.get(`${GRAFANA_URL}/api/prometheus/grafana/api/v1/rules?group_limit=40`, commonRequestParams);
+  console.log("Verifying created rules in Grafana", inputConfig);
+  let prometheusResponse = http.get(`${url}/api/prometheus/grafana/api/v1/rules?group_limit=40&folder_uid=${folderUID}`, {
+    tags: {
+      filter: "folder_uid",
+    },
+    ...commonRequestParams,
+  });
   console.log(`Prometheus rules API response status: ${prometheusResponse.status}`);
   let prometheusData = JSON.parse(prometheusResponse.body);
   console.log(`Prometheus rules API response body: ${prometheusResponse.body}`);
@@ -82,8 +110,8 @@ export default function ({ groups, input_config }) {
   expect(testFolderGroups.length).toBe(groups.length);
 }
 
-export function teardown({ groups, input_config }) {
+export function teardown({ commonRequestParams, url }) {
   // delete the created folder and its contents
   console.log("Tearing down test data in Grafana");
-  deleteFolder(folderUID);
+  deleteFolder(url, folderUID, commonRequestParams);
 }
