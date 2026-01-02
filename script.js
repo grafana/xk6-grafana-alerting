8605ef5844abc77448d213359d0dae5ace2bda89
Santiago Hernández <santiagohernandez.1997@gmail.com>
2026-01-02 14:44:08 +0100

    update alerting package

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ import { GenerateGroups } from "k6/x/grafana-alerting";
 // This should create a folder and upload rules using the default settings.
 export default function () {
   let output = GenerateGroups({
-    alertRuleCount: 1,
-
-    uploadConfig: {
-      grafanaURL: "http://localhost:3000",
-      username: "admin",
-      password: "admin",
-      // token: "test-token",
-      orgId: 1,
-    },
+    numAlerting: 1,
+    numRecording: 5,
+    groupsPerFolder: 5,
+    rulesPerGroup: 5,
+    grafanaURL: "http://localhost:3000",
+    username: "admin",
+    password: "admin",
+    orgId: 1,
+    nuke: true,
   });
 
   console.log("Input config:", output.input_config);
